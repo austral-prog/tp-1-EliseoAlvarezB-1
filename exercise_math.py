d31e08350bb9d8ce96ad16552a5850c41b1b423a
EliseoAlvarezB <ealvarezbrescia@mail.austral.edu.ar>
2025-08-11 12:34:31 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    resta = a - b
+    multi = a * b
+    prom = (a + b) / 2
+    divi = a // b
+    resto = a % b
+    div = a / b
+    print(suma)
+    print(resta)
+    print(multi)
+    print(prom)
+    print(divi)
+    print(resto)
+    print(div)
+math()
